a029be8d9b538a5d3bcdc0a99081b38283f0bad6
bella <bella@bellalee.com>
2022-06-23 16:51:17 +0800

    removed unnecessary things

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -3,7 +3,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import anime from 'animejs';
 
 const links = [
   {
